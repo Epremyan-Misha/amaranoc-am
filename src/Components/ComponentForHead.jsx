5872bd21ecf7a00369b0c3e5aa817a78f5464caf
Misha <misha.epremyan.ait@gmail.com>
2025-07-18 17:49:23 +0400

    New File ComponentForHead.jsx

---
+++ src/Components/ComponentForHead.jsx
@@ -0,0 +1,6 @@
+export function Logo() {
+  return <img className="iconImg" src="photo/icon.png" alt="Icon" />;
+}
+export function SearchInput() {
+  return <input className="input" type="text" placeholder="Որոնում" />;
+}
