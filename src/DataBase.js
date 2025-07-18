6952b497e161a4cc5033a9be16535bed5568e63c
Misha <misha.epremyan.ait@gmail.com>
2025-07-18 13:48:32 +0400

    New DataBase Codes

--- src/DataBase.js
+++ src/DataBase.js
@@ -1,93 +1,174 @@
 let homeImages = [
   {
+    id: 1,
     title: "Ծաղկաձոր",
     people: "15",
     prace: "80,000Դ",
     image: "housesPhoto/caxkadzor.png",
   },
   {
+    id: 2,
     title: "Օհանավան",
     people: "25",
     prace: "90,000Դ",
     image: "housesPhoto/ohanavan.png",
   },
   {
+    id: 3,
     title: "Երևան",
     people: "12",
     prace: "90,000Դ",
     image: "housesPhoto/erevan.png",
   },
   {
+    id: 4,
     title: "Ծաղկաձոր",
     people: "15",
     prace: "140,000Դ",
     image: "housesPhoto/caxkadzor1.png",
   },
   {
+    id: 5,
     title: "Արզնի",
     people: "50",
     prace: "200,000Դ",
     image: "housesPhoto/arzni.png",
   },
   {
+    id: 6,
     title: "Օհանավան",
     people: "40",
     prace: "80,000Դ",
     image: "housesPhoto/ohanavan1.png",
   },
   {
+    id: 7,
     title: "Հրազդան",
     people: "24",
     prace: "150,000Դ",
     image: "housesPhoto/hrazdan.png",
   },
   {
+    id: 8,
     title: "Օհանավան",
     people: "30",
     prace: "100,000Դ",
     image: "housesPhoto/ohanavan2.png",
   },
   {
+    id: 9,
     title: "Ծաղկաձոր",
     people: "30",
     prace: "100,000Դ",
     image: "housesPhoto/caxkadzor2.png",
   },
   {
+    id: 10,
     title: "Գառնի",
     people: "40",
     prace: "150,000Դ",
     image: "housesPhoto/garni.png",
   },
   {
+    id: 11,
     title: "Մրգաշեն",
     people: "20",
     prace: "120,000Դ",
     image: "housesPhoto/mrgashen.png",
   },
   {
+    id: 12,
     title: "Դրախտիկ",
     people: "15",
     prace: "100,000Դ",
     image: "housesPhoto/draxtik.png",
   },
   {
+    id: 13,
     title: "Դիլիջան",
     people: "10",
     prace: "70,000Դ",
     image: "housesPhoto/dilijan.png",
   },
   {
+    id: 14,
     title: "Հրազդան",
     people: "30",
     prace: "120,000Դ",
     image: "housesPhoto/hrazdan1.png",
   },
   {
+    id: 15,
     title: "Ծաղկաձոր",
     people: "40",
     prace: "150,000Դ",
     image: "housesPhoto/caxkadzor3.png",
+  },
+];
+
+export const infoForHeader = [
+  {
+    id: 1,
+    title: "Զեղչեր",
+  },
+  {
+    id: 2,
+    title: "Ծառայություններ",
+  },
+  {
+    id: 3,
+    title: "Մեր մասին",
+  },
+];
+
+export const imgForHeader = [
+  {
+    id: 1,
+    img: "photo/globus.png",
+  },
+  {
+    id: 2,
+    img: "photo/people.png",
+  },
+];
+export const infoHouseForBody = [
+  {
+    id: 1,
+    name: "Դիլիջան 88",
+  },
+  {
+    id: 2,
+    name: "Ծաղկաձոր 63",
+  },
+  {
+    id: 3,
+    name: "Ձորաղբյուր 17",
+  },
+  {
+    id: 4,
+    name: "Ձորաղբյուր 17",
+  },
+  {
+    id: 5,
+    name: "Աշտարակ 15",
+  },
+];
+export const courses = [
+  {
+    id: 1,
+    title: "Դ",
+  },
+  {
+    id: 2,
+    title: "$",
+  },
+  {
+    id: 3,
+    title: "€",
+  },
+  {
+    id: 4,
+    title: "₽",
   },
 ];
 export default homeImages;
