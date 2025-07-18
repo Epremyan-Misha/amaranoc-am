55f84ef061c9a4be024d88082ed02799b1b71b80
Misha <misha.epremyan.ait@gmail.com>
2025-07-18 17:46:39 +0400

    Modified Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,15 +1,15 @@
 import '../index.css';
 import HeaderInfo from './ComponentForHeaderInfo';
-function Head(){
-    return(
-        <>
-        <div className="headDiv">
-            <img className="iconImg" src="photo/icon.png"/>
-            <h4 className="forYellowBorder">Գլխավոր</h4>
-            <HeaderInfo />
-            <input className="input" type="text" placeholder="Որոնում"/>
-        </div>
-        </>
-    )
+import { Logo,SearchInput } from './ComponentForHead';
+function Head() {
+  return (
+    <div className="headDiv">
+      <Logo />
+      <h4 className="forYellowBorder">Գլխավոր</h4>
+      <HeaderInfo />
+      <SearchInput />
+    </div>
+  );
 }
+
 export default Head;
