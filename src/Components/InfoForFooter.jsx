8219dc547e7b7f1129cef5711a57f6c79ba443de
Misha <misha.epremyan.ait@gmail.com>
2025-07-18 17:50:12 +0400

    New File InfoForFooter.jsx

---
+++ src/Components/InfoForFooter.jsx
@@ -0,0 +1,16 @@
+import "../index.css";
+export function InfoH1Footer(){
+    return (
+        <>
+         <h1 className="h1ForFooter">-------- Տեղադրել Հայտարարություն --------</h1>
+       
+        </>
+    )
+} 
+export function InfoPFooter(){
+    return(
+        <>
+        <p className="pForFooter">Մուտքագրեք Ձեր տվյալները նշված դաշտերում և մենք կկապնվենք Ձեզ հետ</p>
+        </>
+    )
+}
